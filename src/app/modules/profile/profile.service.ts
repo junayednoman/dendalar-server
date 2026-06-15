76782a159417df4ae1a7f7e43fad1a966ecf8e9e
junayednoman <junayednoman05@gmail.com>
2026-06-15 10:17:37 +0600

    fix: update data apis logic changed

--- src/app/modules/profile/profile.service.ts
+++ src/app/modules/profile/profile.service.ts
@@ -51,45 +51,74 @@ const updateProfile = async (
 };
 
 const updateActiveLevel = async (userId: string, levelId: string) => {
-  await prisma.level.findUniqueOrThrow({
+  const currentLevel = await prisma.level.findUniqueOrThrow({
     where: { id: levelId },
+  });
+  const nextLevel = await prisma.level.findUniqueOrThrow({
+    where: { index: currentLevel.index + 1 },
   });
   const result = await prisma.userProfile.update({
     where: { authId: userId },
-    data: { activeLevelId: levelId },
+    data: { activeLevelId: nextLevel.id },
   });
   return result;
 };
 
 const updateActiveChapter = async (userId: string, chapterId: string) => {
-  await prisma.chapter.findUniqueOrThrow({
+  const currentChapter = await prisma.chapter.findUniqueOrThrow({
     where: { id: chapterId },
+  });
+
+  const nextChapter = await prisma.chapter.findUniqueOrThrow({
+    where: {
+      levelId_index: {
+        levelId: currentChapter.levelId,
+        index: currentChapter.index + 1,
+      },
+    },
   });
   const result = await prisma.userProfile.update({
     where: { authId: userId },
-    data: { activeChapterId: chapterId },
+    data: { activeChapterId: nextChapter.id },
   });
   return result;
 };
 
 const updateActiveLesson = async (userId: string, lessonId: string) => {
-  await prisma.lesson.findUniqueOrThrow({
+  const currentLesson = await prisma.lesson.findUniqueOrThrow({
     where: { id: lessonId },
+  });
+
+  const nextLesson = await prisma.lesson.findUniqueOrThrow({
+    where: {
+      index: currentLesson.index + 1,
+    },
   });
   const result = await prisma.userProfile.update({
     where: { authId: userId },
-    data: { activeLessonId: lessonId },
+    data: { activeLessonId: nextLesson.id },
   });
   return result;
 };
 
 const updateActiveQuestion = async (userId: string, questionId: string) => {
-  await prisma.question.findUniqueOrThrow({
+  const currentQuestion = await prisma.question.findUniqueOrThrow({
     where: { id: questionId },
+  });
+
+  const nextQuestion = await prisma.question.findUniqueOrThrow({
+    where: {
+      index_type_chapterId_lessonId: {
+        index: currentQuestion.index + 1,
+        type: currentQuestion.type,
+        chapterId: currentQuestion.chapterId,
+        lessonId: currentQuestion.lessonId,
+      },
+    },
   });
   const result = await prisma.userProfile.update({
     where: { authId: userId },
-    data: { activeQuestionId: questionId },
+    data: { activeQuestionId: nextQuestion.id },
   });
   return result;
 };
